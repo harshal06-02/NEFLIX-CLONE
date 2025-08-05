6d517579ca3d1f57ee32fc7004e340f862d517be
Harshal <harshalshende0602@gmail.com>
2025-08-05 18:10:02 +0530

    NC

---
+++ src/components/TitleCards/TitleCard.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useRef } from "react";
+import "./TitleCard.css";
+import cards_data from "../../assets/cards/Cards_data";
+
+const TitleCard = ({ title, category }) => {
+  const cardsRef = useRef();
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+  useEffect(() => {
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  }, []);
+
+  return (
+    <div className="title-card">
+      <h2>{title ? title : "Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt="" />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TitleCard;
